ae01d9fa2e50c7ea6964ec51c983357d0ae27fd9
Paride Legovini <pl@ninthfloor.org>
2017-03-14 14:51:01 +0100

    Add comments to rebranding.sh

--- rebranding.sh
+++ rebranding.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+#####################################
+# Rebrand Signal to Cable.          #
+# This script has to be idempotent. #
+#####################################
+
+# Rebrand strings.
+# When merging from upstream sometimes it's easier
+# to checkout their translations and re-run this script.
+
 find res -iname '*.xml' -exec sed -i '
 s/Signal /Cable /g;
 s/Signal,/Cable,/g;
